a387c53f91f57a0e0809b3812e57a853352d716a
yuuzzzo <yuzzoempresarial@gmail.com>
2024-07-03 16:00:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const nomes = ["Elton", "David", "Rubens", "Felipe", "Alexandre", "Geovanne", "Vinicius", "Danilo"];
 const diasSemana = ["Segunda-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira"];
-const resultado = {};
+let resultado = {};
 
 function sortear(lista, excluido) {
     let sorteado;
@@ -13,6 +13,7 @@ function sortear(lista, excluido) {
 
 function sortearNomes() {
     const nomesRestantes = [...nomes];
+    resultado = {}; // Limpa o resultado anterior
 
     for (const dia of diasSemana) {
         if (dia === "Segunda-feira") {
@@ -21,14 +22,14 @@ function sortearNomes() {
             resultado[dia] = [nomeSorteado];
             nomesRestantes.splice(nomesRestantes.indexOf(nomeSorteado), 1);
         } else if (dia === "Quarta-feira" || dia === "Quinta-feira") {
-            // Sorteia dois nomes para quarta e quinta-feira
+            // Sorteia três nomes para quarta e quinta-feira
             const nome1 = sortear(nomesRestantes);
             nomesRestantes.splice(nomesRestantes.indexOf(nome1), 1);
             const nome2 = sortear(nomesRestantes);
             nomesRestantes.splice(nomesRestantes.indexOf(nome2), 1);
             const nome3 = sortear(nomesRestantes);
-            nomesRestantes.splice(nomesRestantes.indexOf(nome3), 1);
             resultado[dia] = [nome1, nome2, nome3];
+            nomesRestantes.splice(nomesRestantes.indexOf(nome3), 1);
         } else if (dia === "Sexta-feira") {
             // Sorteia um nome para sexta-feira
             const nomeSorteado = sortear(nomesRestantes);
